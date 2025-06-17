8b8a30e25a77b04fa90948770cba2676501084f1
Gagan-repo <gaganroutu2@gmail.com>
2025-06-17 22:53:48 +0530

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -9,7 +9,7 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Optional: run in headless mode
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 
